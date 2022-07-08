a61ea4d5fce9568a3be0c667c3899518483a1be4
mihail8531 <mihail8531@gmail.com>
2022-07-08 19:50:53 +0300

    Add streams getters

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,16 @@
 // authors: mihail8531, danishmakbari
-// FSF 
 
-use gst::prelude::*;
 use derive_more::{Display, Error};
-use std::env;
+use std::{env, fmt::Debug};
 
 #[derive(Debug, Display, Error)]
 #[display(fmt = "Missing element {}", _0)]
 struct MissingElement(#[error(not(source))] &'static str);
 
-#[derive(Debug, Display, Error)]
-#[display(fmt = "Received error from {}: {} (debug: {:?})", src, error, debug)]
-struct ErrorMessage {
-    src: String,
-    error: String,
-    debug: Option<String>,
-    source: glib::Error,
-}
 
 mod rtsp_dyn_pline;
 
 use rtsp_dyn_pline::RTSPPipeline;
-use futures::executor::block_on;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -34,46 +23,15 @@ fn main() {
 
     gst::init();
 
-    let rtsppipeline = match RTSPPipeline::new(uri) {
+    let mut rtsppipeline = match RTSPPipeline::new(uri) {
         Ok(r) => r,
         Err(e) => {eprintln!("Error! {}", e); return},
     };
-    let pipeline = rtsppipeline.pipeline;
-    pipeline.set_state(gst::State::Playing);
 
-    let bus = pipeline
-        .bus()
-        .expect("Pipeline without bus. Shouldn't happen!");
-
-    for msg in bus.iter_timed(gst::ClockTime::NONE) {
-        use gst::MessageView;
-
-        match msg.view() {
-            MessageView::Eos(..) => break,
-            MessageView::Error(err) => {
-                pipeline.set_state(gst::State::Null);
-                {
-                    eprint!("{}", ErrorMessage {
-                        src: msg
-                            .src()
-                            .map(|s| String::from(s.path_string()))
-                            .unwrap_or_else(|| String::from("None")),
-                        error: err.error().to_string(),
-                        debug: err.debug(),
-                        source: err.error(),
-                    });
-                }
-            }
-            MessageView::StateChanged(s) => {
-                println!(
-                    "State changed from {:?}: {:?} -> {:?} ({:?})",
-                    s.src().map(|s| s.path_string()),
-                    s.old(),
-                    s.current(),
-                    s.pending()
-                );
-            }
-            _ => (),
-        }
+    loop {
+        println!("{:?}", rtsppipeline.get_audio_stream());
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
+    
+
 }
--- src/rtsp_dyn_pline.rs
+++ src/rtsp_dyn_pline.rs
@@ -1,38 +1,79 @@
-use futures::Stream;
-use glib::WeakRef;
 use gst::Pipeline;
-use gst::element_error;
 use gst::element_warning;
 use gst::prelude::*;
 
-use std::env;
 
 use anyhow::Error;
 use derive_more::{Display, Error};
 use gst_app::AppSink;
+use gst_app::app_sink::AppSinkStream;
+use gst::MessageView;
 
 #[derive(Debug, Display, Error)]
 #[display(fmt = "Missing element {}", _0)]
 struct MissingElement(#[error(not(source))] &'static str);
 
+#[derive(Debug, Display, Error)]
+#[display(fmt = "Received error from {}: {} (debug: {:?})", src, error, debug)]
+
+struct ErrorMessage {
+    src: String,
+    error: String,
+    debug: Option<String>,
+    source: glib::Error,
+}
+
+fn print_if_err(res: Result<(), glib::error::BoolError>) -> () {
+    match res {
+        Ok(_) => (),
+        Err(e) => glib::g_printerr!("{}", e),
+    }
+}
+
 pub struct RTSPPipeline {
     pub pipeline: Pipeline,
-    pub appsinks: std::sync::Arc<std::sync::Mutex<Vec<gst::Element>>>,
+    audio_stream: Option<AppSinkStream>,
+    video_stream: Option<AppSinkStream>,
 }
 
 impl RTSPPipeline {
+    pub fn get_audio_stream(&mut self) -> Option<&AppSinkStream> {
+        if self.audio_stream.is_some(){
+            return Some(&self.audio_stream.as_ref().unwrap());
+        }
+        match self.pipeline.child_by_name("appsink_audio") {
+            None => None,
+            Some(sink) => {
+                self.audio_stream = Some(sink.dynamic_cast::<AppSink>().unwrap().stream());
+                return Some(&self.audio_stream.as_ref().unwrap());
+            },
+        }
+    }
+
+    pub fn get_video_stream(&mut self) -> Option<&AppSinkStream> {
+        if self.video_stream.is_some(){
+            return Some(&self.video_stream.as_ref().unwrap());
+        }
+        match self.pipeline.child_by_name("appsink_video") {
+            None => None,
+            Some(sink) => {
+                self.video_stream = Some(sink.dynamic_cast::<AppSink>().unwrap().stream());
+                return Some(&self.video_stream.as_ref().unwrap());
+            },
+        }
+    }
+
     pub fn new (uri: &str) -> Result<RTSPPipeline, Error>{
-        let mut rtsp_pipeline = RTSPPipeline {
+        let rtsp_pipeline = RTSPPipeline {
             pipeline: gst::Pipeline::new(None),
-            appsinks: std::sync::Arc::new(std::sync::Mutex::new(Vec::new()))
+            audio_stream: None,
+            video_stream: None,
         };
-        
         let uridecodebin = gst::ElementFactory::make("uridecodebin", None).map_err(|_| MissingElement("uridecodebin"))?;
         uridecodebin.set_property("uri", uri);
         
         rtsp_pipeline.pipeline.add(&uridecodebin)?;
         let pipeline_weak = rtsp_pipeline.pipeline.downgrade();
-        let r_appsinks = std::sync::Arc::clone(&rtsp_pipeline.appsinks);
 
         uridecodebin.connect_pad_added(move |uridecodebin, src_pad| {
             let pipeline = match pipeline_weak.upgrade() {
@@ -40,15 +81,89 @@ impl RTSPPipeline {
                 None => return,
             };
 
-            let appsink = gst::ElementFactory::make("appsink", None).map_err(|_| MissingElement("appsink")).unwrap(); 
-            pipeline.add(&appsink);
-            uridecodebin.link(&appsink);
-           
-            let mut appsinks = r_appsinks.lock().unwrap();
-            appsinks.push(appsink);
-        
+            let (is_audio, is_video) = {
+                let media_type = src_pad.current_caps().and_then(|caps| {
+                    caps.structure(0).map(|s| {
+                        let name = s.name();
+                        (name.starts_with("audio/x-raw"), name.starts_with("video/x-raw"))
+                    })
+                });
+    
+                match media_type {
+                    None => {
+                        element_warning!(
+                            uridecodebin,
+                            gst::CoreError::Negotiation,
+                            ("Failed to get media type from pad {}", src_pad.name())
+                        );
+    
+                        return;
+                    }
+                    Some(media_type) => media_type,
+                }
+            };
+            if is_audio {
+                let appsink = gst::ElementFactory::make("appsink", Some("appsink_audio")).map_err(|_| MissingElement("appsink")).unwrap(); 
+                print_if_err(pipeline.add(&appsink));
+                
+                print_if_err(uridecodebin.link(&appsink));
+                
+                print_if_err(appsink.sync_state_with_parent());
+            }
+            if is_video {
+                let appsink = gst::ElementFactory::make("appsink", Some("appsink_video")).map_err(|_| MissingElement("appsink")).unwrap(); 
+                print_if_err(pipeline.add(&appsink));
+                print_if_err(uridecodebin.link(&appsink));
+                print_if_err(appsink.sync_state_with_parent());
+            }
+            print_if_err(uridecodebin.sync_state_with_parent());
         });
         
+        rtsp_pipeline.pipeline.set_state(gst::State::Playing)?;
+
+        let bus = rtsp_pipeline.pipeline
+            .bus()
+            .expect("Pipeline without bus. Shouldn't happen!");
+
+        let pipeline_weak = rtsp_pipeline.pipeline.downgrade();
+        bus.add_watch(move |_, msg| {
+            match msg.view() {
+                MessageView::Eos(..) => glib::Continue(false),
+                MessageView::Error(err) => {
+                    let pipeline = match pipeline_weak.upgrade() {
+                        Some(pipeline) => pipeline,
+                        None => unimplemented!(),
+                    };
+                    match pipeline.set_state(gst::State::Null) {
+                        Ok(_) => (),
+                        Err(e) => glib::g_printerr!("{}", e),
+                    }
+                    {
+                        eprint!("{}", ErrorMessage {
+                            src: msg
+                                .src()
+                                .map(|s| String::from(s.path_string()))
+                                .unwrap_or_else(|| String::from("None")),
+                            error: err.error().to_string(),
+                            debug: err.debug(),
+                            source: err.error(),
+                        });
+                    glib::Continue(false)
+                    }
+                }
+                MessageView::StateChanged(s) => {
+                    println!(
+                        "State changed from {:?}: {:?} -> {:?} ({:?})",
+                        s.src().map(|s| s.path_string()),
+                        s.old(),
+                        s.current(),
+                        s.pending()
+                    );
+                    glib::Continue(true)
+                }
+                _ => glib::Continue(true),
+            }
+        })?;
         Ok(rtsp_pipeline)
     }
 }
